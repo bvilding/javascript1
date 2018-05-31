c535043bbc436439b73f7589475a08325a1b720e
Vinni Hoke <vinni.hoke@gmail.com>
2018-05-30 19:30:21 -0700

    Day 4 Homework Push

--- js/scripts.js
+++ js/scripts.js
@@ -18,21 +18,103 @@ console.log(jk);
 
 // Day two work
 
-var myName = "Vinni";
-let myAge = new Date("12/28/1989");
-const myGoal = "Learn enough Javascript to be a productive member of my development team.";
+// var myName = "Vinni";
+// let myAge = new Date("12/28/1989");
+// const myGoal = "Learn enough Javascript to be a productive member of my development team.";
+//
+// console.log(myName, myAge, myGoal)
+//
+// /*
+// 1. Declare 2 let variables:
+//   a. Favority movie
+//   b. Favorite food
+// 2. Declare 2 const variables
+//   a. Year the movie came out
+//   b. Ingredients of your favorite foods as an array.
+// 3. Alert your favorite food with its Ingredients
+// 4. Confirm the movie and year
+// */
+//
+// let favMovie = "The Great Gatsby";
+// let favFood = "Tacos";
+// const movieRelease = 2013;
+// const favFoodIngredients = ["Carne Asada", "Corn Tortillas", "White Onion", "Cilantro", "Tapatio to Taste"];
+// alert(favFood + favFoodIngredients);
+// confirm(favMovie + movieRelease);
 
-console.log(myName, myAge, myGoal)
+// Day Four work
 
-// this example takes 2 seconds to run
-var start = Date.now();
+// Intro Day 4
 
-console.log("Starting timer...");
-// expected output: starting timer...
+// let number = 5;
 
-setTimeout(function() {
-  var millis = Date.now() - start;
+// number > 10 ? "number is greater" : "number isn't greater";
 
-  console.log("Seconds elapsed = " + Math.floor(millis/2000));
-  // expected output : seconds elapsed = 2
-}, 5000);
+// console.log(number > 10 ? "number is greater" : "number isn't greater")
+
+// let name = false;
+// let email = true;
+
+// if (name){
+//   alert("I have da name rite 'er")
+// }
+
+// else if (email){
+//   console.log("Email's right 'er bruv")
+// }
+
+// else {
+//   console.log("Ain't got nuffin")
+// }
+
+// name ? "I have da name rite 'er" : email ? "No email 'er bruv" : "Ain't got nuffin";
+
+// console.log(name ? "I have da name rite 'er" : email ? "Email's right 'er bruv" : "Ain't got nuffin");
+
+
+// Example 1
+
+// let money = 10;
+// let iAmNotHungry = true;
+
+// if (iAmHungry) {
+//   if (money > 10){
+//     console.log('I am hungry and I have more than 10')
+//   }
+//   else {
+//     console.log('I am hungry and I have less than 10')
+//   }
+// } else {
+//   if (money < 10){
+//     console.log('I am not hungry and I have more than 10')
+//   }
+//   else {
+//     console.log('I am not hungry and I have less than 10')
+//   }
+// }
+
+// Example 2
+
+// iAmHungry ? ((money > 10) ? 'I am hungry and I have more than 10' : 'I am hungry and I have less than 10')
+
+// iAmHungry ? ((money > 10) ? 'I am not hungry and I have more than 10' : 'I am not hungry and I have less than 10')
+
+// console.log(hungry);
+
+// Example 3
+
+// let iAmWearingYellow = false;
+// let number = 24;
+
+// let num = (number = 23 && iAmWearingYellow) ? "All bets on the Lakers" : "All bets on the Bulls" : "Neither";
+// console.log(num);
+
+// Sample 4
+
+var fruits = ['Apple', 'Banana'];
+console.log(fruits.length);
+
+var first = fruits[0];
+
+let arr =['Javascript', 100, [], {}, true];
+console.log(arr[0]); 
