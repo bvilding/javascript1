48ee0660940901c027330fe49bde1c2c7392930f
Vinni Hoke <vhoke@Vinnis-MacBook-Air.local>
2018-06-13 19:09:42 -0700

    Day 7 - Needs to be replaced

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,14 @@ let h1 = document.querySelector('.h1');
 
 button.addEventListener('click', e => {
   alert('Hey, good job!!');
+  e.target.textContent = "Goodbye cats!!";
 })
 
 div.addEventListener('mouseenter', e => {
-  e.target.style.backgroundColor = "#aa0000"
+  e.target.style.backgroundColor = "#aa0000";
   console.log(e);
 })
+
+h1.addEventListener('mouseleave'), e => {
+  e.target.style.color = '#00ffff';
+}
