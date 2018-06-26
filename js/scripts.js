b1557f1527a2b78fd5116657f00d1f51ae6e2a05
bvilding <vhoke@Vinnis-MacBook-Air.local>
2018-06-25 20:08:57 -0700

    No idea what this was about...

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,15 @@
-  //JSON is Java Script Object Notation
-   asdf
+//Plain HTTPS request and json returned.
+function queryRandomUserAPI() {
+  var url = 'https://randomuser.me/api/?results=10';
+  fetch(url) // Call the fetch function passing the url of the API as a parameter
+    .then(function(resp){
+      return resp.json()// Transform the data into json
+    })
+    .then(function(data) {
+      console.log(data);
+      //How can we display this data?
+    })
+    .catch(function() {
+        // This is where you run code if the server returns any errors
+    });
+};
